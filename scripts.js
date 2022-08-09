3f8f58700176ee0e2710e2fd6f06ad5a1c2e5988
Sam L <79550570+sjlee18@users.noreply.github.com>
2022-08-08 20:25:46 -0700

    added main function and variables
    billAmount, tipAmount, peopleAmount

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
+/*
+//Initialize values to 0
+let billAmount = 0;
+let tipAmount = 0;
+let peopleAmount = 0;
 
+//Main function here
+function justTheTipCalculator() {
+  let billAmount = document.querySelector("billamount").value;
+  let tipAmount = document.querySelector("tipamount").value;
+  let peopleAmount = document.querySelector("peopleamount").value;
+*/
 let calcButton = document.getElementById("calculateTip");
 
 calcButton.onclick = function () {
