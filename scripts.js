04aad29c37671aa539767fc0806f1b18aceb127c
Sam L <79550570+sjlee18@users.noreply.github.com>
2022-08-09 16:37:47 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,10 @@
 let billAmt = 0;
 let tipAmt = 0;
 let numOfGuests = 0;
+let tenPercent = 10;
+let twentyPercent = 20;
+let twentyFivePercent = 25;
+let fifteenPercent = 15;
 
 //Main function here
 function justTheTipCalculator() {
@@ -15,15 +19,25 @@ function justTheTipCalculator() {
 
 //error handling
 //if any text fields = empty, prompt error message
+//if (tipAmt === "" || billAmt === "") {
+//$(".errorMsg hidden").text("Please input values for Bill and select a tip amount").fadeIn();
+//}
 
 //implement numofGuests 
-//if numOfGuest = empty and numOfGuests less than 1, prompt error msg
-// else 
+//if numOfGuest === "" || numOfGuests < 1) {
+//$(".errorMsg hidden").text("Please enter positive number for number of peeps").fadeIn();
+//} else {
+//document.querySelector('#numOfGuests').value;
+//}
 
 //implement calculation of tip from (bill amt * tipamt selected) / numofGuests
 //let totalTipAmount = (billAmt * tipamt selected) / numOfGuests;
 
 //convert total to dollar decimal amount
+//totalTipAmount = Math.round(totalTipAmount * 100) / 100;
+//totalTipAmount = totalTipAmont.tofixed(2);
+
+
 
 
 //}
